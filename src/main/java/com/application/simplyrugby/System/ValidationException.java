3321a237934fc5820a2a1af9964ab892959a989e
Erik McS <klaelfr60@outlook.com>
2023-05-10 11:54:34 +0100

    working on squad deletion functions

--- src/main/java/com/application/simplyrugby/System/ValidationException.java
+++ src/main/java/com/application/simplyrugby/System/ValidationException.java
@@ -1,10 +1,8 @@
 package com.application.simplyrugby.System;
-
 /**
  * Custom Exception class to create error messages used by the app.
  */
 public class ValidationException extends Exception{
-
     public ValidationException(String message){
         super(message);
     }
